0c426874fa83201c9f35bd0a01e99d4932e93dd0
GregVernon <greg.j.vernon@gmail.com>
2021-11-11 19:52:53 -0700

    Create submission_commands.bat

---
+++ lectures/LinearSystems/Sims/submission_commands.bat
@@ -0,0 +1,24 @@
+C:\SIMULIA\Commands\abaqus.bat job=Beam-Beam2-FixedFixed        input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=Beam-Beam2-FreeFree          input=LinearSystemExtraction_FreeFree.inp
+
+C:\SIMULIA\Commands\abaqus.bat job=Beam-Beam3-FixedFixed        input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=Beam-Beam3-FreeFree          input=LinearSystemExtraction_FreeFree.inp
+
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Hex8-FixedFixed    input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Hex8-FreeFree      input=LinearSystemExtraction_FreeFree.inp
+
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Hex20-FixedFixed   input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Hex20-FreeFree     input=LinearSystemExtraction_FreeFree.inp
+
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Tet4-FixedFixed    input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Tet4-FreeFree      input=LinearSystemExtraction_FreeFree.inp
+
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Tet10-FixedFixed   input=LinearSystemExtraction_FixedFixed.inp
+C:\SIMULIA\Commands\abaqus.bat job=SolidBeam-Tet10-FreeFree     input=LinearSystemExtraction_FreeFree.inp
+
+#########
+#########
+
+C:\SIMULIA\Commands\abaqus.bat job=Poor_Pave_QuadMesh            input=LinearSystemExtraction_MeshQuality.inp
+C:\SIMULIA\Commands\abaqus.bat job=Quality_Pave_QuadMesh         input=LinearSystemExtraction_MeshQuality.inp
+C:\SIMULIA\Commands\abaqus.bat job=Quality_SemiStruct_QuadMesh   input=LinearSystemExtraction_MeshQuality.inp
